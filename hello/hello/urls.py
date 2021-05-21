45d0a25758ea02e7d1ac622540c1b9a73edab482
Dmitry Stepanov <stpnv.dmtry@gmail.com>
2021-05-21 11:49:05 +0600

    Роуты для API второй версии зарегистрированы в urls.py. Вынесены неймспейсы для API-роутов

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -23,11 +23,16 @@ from django.conf import settings
 HOMEPAGE_URL = 'articles/'
 
 
+api_urls = [
+    path('v1/', include('api_v1.urls')),
+    path('v2/', include('api_v2.urls')),
+]
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('article.urls')),
-    path('notes/', include('notes.urls')),
     path('accounts/', include('accounts.urls')),
-    path('api_v1/', include('api_v1.urls')),
+    path('api/', include(api_urls)),
     path('', RedirectView.as_view(url=HOMEPAGE_URL, permanent=True)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
